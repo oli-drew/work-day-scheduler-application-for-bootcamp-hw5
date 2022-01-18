a991349711998daf4e584974e27b70eb2b07d12d
Oli Drew <oli.drew92@gmail.com>
2022-01-18 11:41:55 +0000

    Display day using Momentjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,6 @@
-console.log("Linked");
+"use strict";
+// - Select page elements by Id -
+// Current day element
+const currentDay = $("#currentDay");
+// Set current day using momentjs
+currentDay.text(moment().format("dddd"));
