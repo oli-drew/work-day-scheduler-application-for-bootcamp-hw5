a3bd5fdbbcd68737ccfb3f7699a358ce02efdf38
Oli Drew <oli.drew92@gmail.com>
2022-01-18 13:55:06 +0000

    Display day and date using Momentjs

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,5 +2,10 @@
 // - Select page elements by Id -
 // Current day element
 const currentDay = $("#currentDay");
-// Set current day using momentjs
-currentDay.text(moment().format("dddd"));
+
+// Get current day using Moment.js library
+const nowDay = moment().format("dddd");
+// Get current day using Moment.js library
+const nowDate = moment().format("DD/MM/YY");
+// Add current day and date to currentDay element
+currentDay.text(`${nowDay} (${nowDate})`);
