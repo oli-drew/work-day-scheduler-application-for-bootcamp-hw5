8c15b14c6bbc01c91130f70efc16f5e5342edd49
Oli Drew <oli.drew92@gmail.com>
2022-01-18 15:18:47 +0000

    Function to check if past, present, future

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,6 +6,20 @@ const currentDay = $("#currentDay");
 // Get current day using Moment.js library
 const nowDay = moment().format("dddd");
 // Get current day using Moment.js library
-const nowDate = moment().format("DD/MM/YY");
+const nowDate = moment().format("DD/MM/YYYY");
 // Add current day and date to currentDay element
 currentDay.text(`${nowDay} (${nowDate})`);
+
+// Check if date is past/present/future
+const pastFuturePresent = (inputDate) => {
+  const date = moment(inputDate);
+  const todayStart = moment().startOf("day");
+  const todayEnd = moment().endOf("day");
+  if (date.isBefore(todayStart)) {
+    return "Past";
+  } else if (date.isAfter(todayEnd)) {
+    return "Future";
+  } else {
+    return "Present";
+  }
+};
