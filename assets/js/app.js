2aeec7e38cf96b24e3bd682598a0e5766629d6da
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-01-22 23:31:40 +0000

    html timeblocks template

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,20 +6,35 @@ const currentDay = $("#currentDay");
 // Get current day using Moment.js library
 const nowDay = moment().format("dddd");
 // Get current day using Moment.js library
-const nowDate = moment().format("DD/MM/YYYY");
+// const nowDate = moment().format("DD/MM/YYYY");
+// const nowDate = moment().format("ll");
+const nowDate = moment().format("MMMM Do");
 // Add current day and date to currentDay element
-currentDay.text(`${nowDay} (${nowDate})`);
+currentDay.text(`${nowDay}, ${nowDate}`);
 
-// Check if date is past/present/future
-const pastFuturePresent = (inputDate) => {
-  const date = moment(inputDate);
-  const todayStart = moment().startOf("day");
-  const todayEnd = moment().endOf("day");
-  if (date.isBefore(todayStart)) {
-    return "Past";
-  } else if (date.isAfter(todayEnd)) {
-    return "Future";
+// // Check if date is past/present/future
+// const pastFuturePresent = (inputDate) => {
+//   const date = moment(inputDate);
+//   const todayStart = moment().startOf("day");
+//   const todayEnd = moment().endOf("day");
+//   if (date.isBefore(todayStart)) {
+//     return "Past";
+//   } else if (date.isAfter(todayEnd)) {
+//     return "Future";
+//   } else {
+//     return "Present";
+//   }
+// };
+
+// Check hour
+const pastPresentFuture = (inputHour) => {
+  const currentHour = moment().hour();
+  console.log(`hour: ${currentHour}`);
+  if (inputHour < currentHour) {
+    console.log("Past");
+  } else if (inputHour > currentHour) {
+    console.log("Future");
   } else {
-    return "Present";
+    console.log("Present");
   }
 };
