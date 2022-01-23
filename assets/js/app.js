2fdaff4de8371c9e386d0a650fab520acb67a163
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-01-23 14:17:19 +0000

    Capture user input when save clicked

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,12 +20,20 @@ const rowColour = (inputHour) => {
   }
 };
 
+// Save task
+const saveTask = (e) => {
+  const inputID = e.target.getAttribute("data-hour");
+  console.log(inputID);
+  const inputValue = document.querySelector(`#input-${inputID}`).value;
+  console.log(inputValue);
+};
+
 // Timeblocks table
 const timeblocks = document.querySelector("#timeblocks");
 
 // Create timeblocks dynamically
 const createTimeblocks = () => {
-  let startHour = 0;
+  let startHour = 9;
   const dayLength = 8;
   for (let i = 0; i <= dayLength; i++) {
     // Convert start hour to time
@@ -49,6 +57,7 @@ const createTimeblocks = () => {
     // User task Input
     const taskInput = document.createElement("input");
     taskInput.setAttribute("type", "text");
+    taskInput.setAttribute("id", `input-${startHour}`);
     taskInput.classList.add("form-control");
     inputCol.append(taskInput);
     // Save column
@@ -57,8 +66,11 @@ const createTimeblocks = () => {
     row.append(saveCol);
     // Save button
     const saveBtn = document.createElement("button");
+    saveBtn.addEventListener("click", saveTask);
+    saveBtn.setAttribute("data-hour", `${startHour}`);
     saveBtn.classList.add("btn", "btn-outline-secondary");
-    saveBtn.innerHTML = '<i class="fas fa-save"></i>';
+    // saveBtn.innerHTML = '<i class="fas fa-save"></i>';
+    saveBtn.innerHTML = "Save";
     saveCol.append(saveBtn);
     // Increment start hour
     startHour++;
@@ -66,3 +78,57 @@ const createTimeblocks = () => {
 };
 
 createTimeblocks();
+
+// const buttons = document.getElementsByClassName("saveBtn");
+// buttons.forEach((btn) => {
+//   btn.addEventListener("click", console.log("hi"));
+// });
+
+// // Capture user input
+// const testSubmit = (e) => {
+//   // // Prevent form submit default action
+//   // e.preventDefault();
+//   const testInput = document.querySelector("#test");
+//   // Initials value
+//   console.log(testInput.value);
+// };
+
+// // Test submit button
+// const testBtn = document.querySelector("#testBtn");
+// testBtn.addEventListener("click", testSubmit);
+
+// // Array to store tasks
+// const dailyTasks = [];
+
+// // Function to get previous tasks
+// const getTasks = () => {
+//   const tasks = localStorage.getItem("tasks");
+//   // If not null
+//   if (tasks) {
+//     // Return the tasks as an object
+//     return JSON.parse(tasks);
+//   } else {
+//     // Return empty object
+//     return {};
+//   }
+// };
+
+// // Save to local storage
+// // Function to add task
+// const addTask = (hour, task) => {
+//   // Create new task
+//   const newTask = { [hour]: task };
+//   // Get the previous tasks
+//   const prevTasks = getTasks();
+//   // Assign new tasks to old tasks object
+//   const updateTasks = Object.assign(newTask, prevTasks);
+//   // Save the tasks to local storage
+//   localStorage.setItem("tasks", JSON.stringify(updateTasks));
+//   // Open high scores table
+//   displayTasks();
+// };
+
+// // Display tasks
+// const displayTasks = () => {
+//   //
+// };
