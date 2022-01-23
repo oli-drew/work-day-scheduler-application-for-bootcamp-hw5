acd35f73c4e40aed3634296c2efdd07fe14eeabf
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-01-23 01:17:26 +0000

    Add favicon and button save icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,62 +1,21 @@
 "use strict";
-// - Select page elements by Id -
 // Current day element
 const currentDay = $("#currentDay");
-
 // Get current day using Moment.js library
 const nowDay = moment().format("dddd");
 // Get current day using Moment.js library
-// const nowDate = moment().format("DD/MM/YYYY");
-// const nowDate = moment().format("ll");
 const nowDate = moment().format("MMMM Do");
 // Add current day and date to currentDay element
 currentDay.text(`${nowDay}, ${nowDate}`);
 
-// // Check if date is past/present/future
-// const pastFuturePresent = (inputDate) => {
-//   const date = moment(inputDate);
-//   const todayStart = moment().startOf("day");
-//   const todayEnd = moment().endOf("day");
-//   if (date.isBefore(todayStart)) {
-//     return "Past";
-//   } else if (date.isAfter(todayEnd)) {
-//     return "Future";
-//   } else {
-//     return "Present";
-//   }
-// };
-
-// Check hour
-const pastPresentFuture = (inputHour) => {
-  const currentHour = moment().hour();
-  console.log(`hour: ${currentHour}`);
-  if (inputHour < currentHour) {
-    console.log("Past");
-  } else if (inputHour > currentHour) {
-    console.log("Future");
-  } else {
-    console.log("Present");
-  }
-};
-
-// {/* <tr class="table-secondary">
-//   <th scope="row" class="col-1 text-center align-middle">9 am</th>
-//   <td class="col-10"><input class="form-control" type="text"></td>
-//   <td class="col-1 text-center"><button class="btn btn-outline-success">Save</button></td>
-// </tr> */}
-
 // Set colour based on time
 const rowColour = (inputHour) => {
   const currentHour = moment().hour();
-  console.log(`hour: ${currentHour}`);
   if (inputHour < currentHour) {
-    console.log("Past");
     return "table-secondary";
   } else if (inputHour > currentHour) {
-    console.log("Future");
     return "table-primary";
   } else {
-    console.log("Present");
     return "table-success";
   }
 };
@@ -67,7 +26,8 @@ const timeblocks = document.querySelector("#timeblocks");
 // Create timeblocks dynamically
 const createTimeblocks = () => {
   let startHour = 0;
-  for (let i = 0; i <= 8; i++) {
+  const dayLength = 8;
+  for (let i = 0; i <= dayLength; i++) {
     // Convert start hour to time
     const hour = moment().hour(startHour).format("hh a");
     // Create a new table row
@@ -98,7 +58,7 @@ const createTimeblocks = () => {
     // Save button
     const saveBtn = document.createElement("button");
     saveBtn.classList.add("btn", "btn-outline-secondary");
-    saveBtn.textContent = "Save";
+    saveBtn.innerHTML = '<i class="fas fa-save"></i>';
     saveCol.append(saveBtn);
     // Increment start hour
     startHour++;
