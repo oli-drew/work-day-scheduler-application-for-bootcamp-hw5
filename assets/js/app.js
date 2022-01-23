e013d36fbfadbf42331001250ee6a14ebeba79b1
oli-drew <89919025+oli-drew@users.noreply.github.com>
2022-01-23 00:15:57 +0000

    Dynamically create the timeblocks

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,3 +38,52 @@ const pastPresentFuture = (inputHour) => {
     console.log("Present");
   }
 };
+
+// {/* <tr class="table-secondary">
+//   <th scope="row" class="col-1 text-center align-middle">9 am</th>
+//   <td class="col-10"><input class="form-control" type="text"></td>
+//   <td class="col-1 text-center"><button class="btn btn-outline-success">Save</button></td>
+// </tr> */}
+
+// Timeblocks table
+const timeblocks = document.querySelector("#timeblocks");
+
+// Create timeblocks dynamically
+const createTimeblocks = () => {
+  let startHour = 9;
+  for (let i = 0; i <= 8; i++) {
+    // Convert start hour to time
+    const hour = moment().hour(startHour).format("hh a");
+    // Create a new table row
+    const row = document.createElement("tr");
+    row.classList.add("table-secondary");
+    timeblocks.append(row);
+    // Time column
+    const timeCol = document.createElement("th");
+    timeCol.classList.add("col-1", "text-center", "align-middle");
+    timeCol.textContent = hour;
+    row.append(timeCol);
+    // Input column
+    const inputCol = document.createElement("td");
+    inputCol.classList.add("col-10");
+    row.append(inputCol);
+    // User task Input
+    const taskInput = document.createElement("input");
+    taskInput.setAttribute("type", "text");
+    taskInput.classList.add("form-control");
+    inputCol.append(taskInput);
+    // Save column
+    const saveCol = document.createElement("td");
+    saveCol.classList.add("col-1", "text-center");
+    row.append(saveCol);
+    // Save button
+    const saveBtn = document.createElement("button");
+    saveBtn.classList.add("btn", "btn-outline-success");
+    saveBtn.textContent = "Save";
+    saveCol.append(saveBtn);
+    // Increment start hour
+    startHour++;
+  }
+};
+
+createTimeblocks();
